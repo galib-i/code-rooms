a1884a074f9abc886a3ce132c12901eca0a67936
Galib <60702760+galib-i@users.noreply.github.com>
2024-09-15 15:28:59 +0100

    Separate authentication from main

---
+++ auth.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, render_template, url_for, request, redirect
+from flask_login import login_user, login_required, logout_user, current_user
+from pymongo.errors import DuplicateKeyError
+
+from db_connection import save_user, get_user
+
+auth_bp = Blueprint('auth', __name__)
+
+
+@auth_bp.route("/register/", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        try:
+            save_user(username=username, email=email, password=password)
+            return redirect(url_for("auth.login"))
+        except DuplicateKeyError:
+            return render_template("register.html", error="Username already exists!")
+
+    return render_template("register.html")
+
+
+@auth_bp.route("/login/", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password_input = request.form.get("password")
+        user = get_user(username)
+
+        if user and user.check_password(password=password_input):
+            login_user(user)
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html", error="Error! Try again.")
+
+    return render_template("login.html")
+
+
+@auth_bp.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template
 from flask_socketio import SocketIO
-from pymongo.errors import DuplicateKeyError
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager
 
-from db_connection import save_user, get_user
+from auth import auth_bp
+from db_connection import get_user
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app)
 login_manager = LoginManager()
-login_manager.login_view = 'login'
+login_manager.login_view = "auth.login"
 login_manager.init_app(app)
+
+app.register_blueprint(auth_bp)
 
 
 @app.route("/")
@@ -18,54 +20,9 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/register/", methods=["GET", "POST"])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for("home"))
-
-    if request.method == "POST":
-        username = request.form.get("username")
-        email = request.form.get("email")
-        password = request.form.get("password")
-
-        try:
-            save_user(username=username, email=email, password=password)
-            return redirect(url_for("login"))
-        except DuplicateKeyError:
-            return render_template("register.html", error="Username already exists!")
-
-    return render_template("register.html")
-
-
-@app.route("/login/", methods=["GET", "POST"])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-
-    if request.method == "POST":
-        username = request.form.get("username")
-        password_input = request.form.get("password")
-        user = get_user(username)
-
-        if user and user.check_password(password=password_input):
-            login_user(user)
-            return redirect(url_for("home"))
-        else:
-            return render_template("login.html", error="Error! Try again.")
-
-    return render_template("login.html")
-
-
 @login_manager.user_loader
 def load_user(username):
     return get_user(username)
-
-
-@app.route("/logout/")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
