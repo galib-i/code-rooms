a981005478147455c7600a1649983f34b6f56f52
Galib <60702760+galib-i@users.noreply.github.com>
2024-09-15 14:12:03 +0100

    Complete registration

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_socketio import SocketIO
+from pymongo.errors import DuplicateKeyError
+
+from db_connection import save_user
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
@@ -11,13 +14,24 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login/")
 def login():
     return render_template("login.html")
 
 
-@app.route("/register")
+@app.route("/register/", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        try:
+            save_user(username=username, email=email, password=password)
+            return redirect(url_for("login"))
+        except DuplicateKeyError:
+            return render_template("register.html", error="Username already exists!")
+
     return render_template("register.html")
 
 
