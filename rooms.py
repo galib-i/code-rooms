a126fa84da88c3049b8d1a3ed653fdaeb0227b1e
Galib <60702760+galib-i@users.noreply.github.com>
2024-09-18 13:56:31 +0100

    Add editor and output functionality

--- rooms.py
+++ rooms.py
@@ -8,26 +8,29 @@ from flask_login import login_required
 rooms_bp = Blueprint("rooms", __name__)
 
 
-@rooms_bp.route("/view-room/", methods=["GET", "POST"])
+@rooms_bp.route("/view-room/", methods=["GET"])
 @login_required
 def view_room():
-    if request.method == "POST":
-        code = request.json.get('code')
-        old_stdout = sys.stdout
-        redirected_output = StringIO()
-        sys.stdout = redirected_output
-
-        try:
-            exec(code)
-        except Exception as e:
-            output = str(e)
-        else:
-            output = redirected_output.getvalue()
-        finally:
-            sys.stdout = old_stdout
-
-        return jsonify({'output': output})
     return render_template("view-room.html")
+
+
+@rooms_bp.route("/run-code", methods=["POST"])
+def run_code():
+    code = request.json.get("code")
+    old_stdout = sys.stdout
+    redirected_output = StringIO()
+    sys.stdout = redirected_output
+
+    try:
+        exec(code)
+    except Exception as e:
+        output = str(e)
+    else:
+        output = redirected_output.getvalue()
+    finally:
+        sys.stdout = old_stdout
+
+    return jsonify({"output": output})
 
 
 @rooms_bp.route("/join-room/", methods=["GET", "POST"])
