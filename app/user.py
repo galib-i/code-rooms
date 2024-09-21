21a87fe19dc62824002c2849d015ef89f3583418
Galib <60702760+galib-i@users.noreply.github.com>
2024-09-21 19:14:39 +0100

    Tidy up user.py

--- app/user.py
+++ app/user.py
@@ -2,6 +2,7 @@ from werkzeug.security import check_password_hash
 
 
 class User:
+    """Holds the current user data"""
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
@@ -9,14 +10,20 @@ class User:
 
     @staticmethod
     def is_authenticated():
-        return True
-
-    @staticmethod
-    def is_active():
+        """Checks if the user is logged in"""
         return True
 
     def get_id(self):
+        """Returns the username - overrides method for Flask-Login"""
         return self.username
 
     def check_password(self, password):
+        """Checks if the entered password matches the stored one
+
+        Args:
+            password (str): plaintext password
+
+        Returns:
+            bool: True if the password matches, False otherwise
+        """
         return check_password_hash(pwhash=self.password, password=password)
